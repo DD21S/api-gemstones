c6c1992e20f9dfee30541252a36c5435bded1c37
Deibys <atom32@tutanota.com>
2022-06-15 17:34:26 -0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'gemstones/index'
-  get 'gemstones/show'
-  get 'gemstones/create'
-  get 'gemstones/update'
-  get 'gemstones/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "gemstones#index"
+
+  resources :gemstones
 end
